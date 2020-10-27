45ed1c08bd99cdbdf4b525ad8dd1c94fc878cc99
AlexV2204 <alexandre.vilbert1@gmail.com>
2020-10-27 14:32:26 +0100

    props et plus d'utilisateurs

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div>
       <Contact name="Maddie Kisard" avatar="https://randomuser.me/api/portraits/women/12.jpg" online />
+      <Contact name="Jessie Nehr" avatar="https://randomuser.me/api/portraits/men/83.jpg" online />
+      <Contact name="Hugh Janus" avatar="https://randomuser.me/api/portraits/men/20.jpg" />
     </div>
   );
 }
