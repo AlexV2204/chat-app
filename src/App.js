702c1a685b0d3132a9efa7ec33c03e96ad38ee8e
AlexV2204 <alexandre.vilbert1@gmail.com>
2020-10-27 14:05:50 +0100

    Création d'une liste de contacts, mapping -> version 1.2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Contact from "./components/Contact";
+import users from './components/ContactList';
 
 function App() {
   return (
+
     <div>
-      <Contact name="Maddie Kisard" avatar="https://randomuser.me/api/portraits/women/12.jpg" online />
+      {users.map(user => (
+        <Contact key={user} name={user.name} avatar={user.avatar} online={user.online} />
+      ))}
+
     </div>
   );
 }
---
+++ src/components/ContactList.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const users = [
+    {
+        name: 'Maddie Kisard',
+        avatar: 'https://randomuser.me/api/portraits/women/12.jpg',
+        online: false
+    },
+    {
+        name: 'Jessie Nehr',
+        avatar: 'https://randomuser.me/api/portraits/men/83.jpg',
+        online: true
+    },
+    {
+        name: 'Hugh Janus',
+        avatar: 'https://randomuser.me/api/portraits/men/20.jpg',
+        online: true
+    },
+    {
+        name: 'Norma Stitz',
+        avatar: 'https://randomuser.me/api/portraits/women/76.jpg',
+        online: false
+    },
+    {
+        name: 'Olie Fok',
+        avatar: 'https://randomuser.me/api/portraits/women/70.jpg',
+        online: true
+    }
+];
+
+export default users;
